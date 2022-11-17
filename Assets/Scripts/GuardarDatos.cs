c9fb6a7dae4bb8c117a82538657f2e128e364456
Pantantey <102394928+Pantantey@users.noreply.github.com>
2022-11-17 14:43:56 -0600

    Tabla de puntuaciones terminada

--- Assets/Scripts/GuardarDatos.cs
+++ Assets/Scripts/GuardarDatos.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GuardarDatos : MonoBehaviour
 {
     //datos para crear la tabla
-    private TextMeshProUGUI textMesh;
     public bool derrotaVictoria = false;
+    public bool final = true;
+    public ScoreManager scoreManager;
 
     //nombre del input
     private string namePlayer;
     private int nivel;
     public bool subirNivel = true;
 
+    private string finalPrefsName = "final";
     private string nivelPrefsName = "nivelMax";
     private string namePrefsName = "nombreJugador";
 
@@ -21,33 +25,26 @@ public class GuardarDatos : MonoBehaviour
     private void Awake()
     {
         LoadData();
+
         if (subirNivel)
         {
             nivel++;
         }
+
+        
     }
 
     private void Start()
     {
-        if (derrotaVictoria)
+        if (derrotaVictoria && final)
         {
-            //iniciar textMesh para la tabla
-            textMesh = GetComponent<TextMeshProUGUI>();
-        }
-        
-    }
-    private void Update()
-    {
-        if (derrotaVictoria)
-        {
-            //actualizar tabla
-            textMesh.text = namePlayer+"    nivel: "+nivel.ToString("0");
+            scoreManager.AddScore(new Score(namePlayer, nivel));
         }
     }
 
     private void OnDestroy()
     {
-        SaveData();
+            SaveData();
     }
 
     //****** Leer nombre del string y guardar nombre y nivel
@@ -58,23 +55,33 @@ public class GuardarDatos : MonoBehaviour
     public void reiniciarNiveles()
     {
         nivel = 0;
+        final = true;
+    }
+
+    public void ActualizarTabla(string sceneName)
+    {
+        final = false;
+        SceneManager.LoadScene(sceneName);
     }
 
     private void SaveData()
     {
         PlayerPrefs.SetString(namePrefsName, namePlayer);
         PlayerPrefs.SetInt(nivelPrefsName, nivel);
+        PlayerPrefs.SetInt(finalPrefsName, Convert.ToInt32 (final));
 
         print("guardando nombre: " + namePlayer + "\n");
         print("guardando nivel: " + nivel);
-
+        print("guardando final: " + derrotaVictoria);
     }
     private void LoadData()
     {
         namePlayer = PlayerPrefs.GetString(namePrefsName, "NoName");
         nivel = PlayerPrefs.GetInt(nivelPrefsName);
+        final = Convert.ToBoolean(PlayerPrefs.GetInt(finalPrefsName));
 
         print("cargando nombre: " + namePlayer + "\n");
         print("cargando nivel: " + nivel);
+        print("cargando final: " + final);
     }
 }
